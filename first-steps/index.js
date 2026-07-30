1a88cedcaf0921a9d6b85485b77d4b74c83971e5
Carlos F Arboleda G <arbofercho@gmail.com>
2026-07-30 00:59:40 -0500

    fix: update interaction creation logic and adjust script paths in package.json

--- first-steps/index.js
+++ first-steps/index.js
@@ -3,11 +3,19 @@ import { GoogleGenAI } from "@google/genai";
 const client = new GoogleGenAI({});
 
 async function createAgent() {
-    const interaction = await client.interactions.create({
+    const interaction = await client.interactions.create(
+      {
         agent: "antigravity-preview-05-2026",
-        input: "Look for the USD to COP exchange rate for the last 10 days, analyze the behavior and generate a forecast for the next 3 days, finally save the results as a PDF.",
+        input:
+          "Look for the USD to COP exchange rate for the last 10 days, analyze the behavior and generate a forecast for the next 3 days, finally save the results as a PDF.",
         environment: "remote",
-    }, { timeout: 300000 });
+        background: true,
+      },
+      //   { timeout: 300000 },
+    );
+
+    const interac = await client.interactions.get(interaction.id);
+    console.log(interac);
 
     console.log(interaction.id, interaction.output_text);
     console.log(interaction);
@@ -23,5 +31,5 @@ async function listAgents() {
     }
 }
 
-// createAgent();
-listAgents();
+createAgent();
+// listAgents();
